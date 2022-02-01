ace0921e681c42451891cb26ce7f67c568922d90
Mari <sherst.neva.maria@ya.ru>
2022-02-01 15:40:02 +0300

    Update _id.vue

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,22 +1,25 @@
 <template>
 	<v-container>
 		<!-- <div class="text-h2"> {{ $card.id }} </div> -->
+		<Photo v-for="photo in photos" :key="photo.id" :photo="photo"></Photo>
 	</v-container>
 </template>
 
 <script>
 import Card from '@/components/Card'
+import Photo from '@/components/Photo'
 
 export default {
 	components: {
-		Card
+		Card,
+		Photo
 	},
 	data: () => ({
 		cardsList: [],
 		photos: []
 	}),
-	async asyncData({$axios, params}) {
-		this.cardsList = await this.$axios.$get("/articles.json/" + params.id),
+	async mounted() {
+		// this.cardsList = await this.$axios.$get("/articles.json/" + params.id),
 		this.photos = await this.$axios.$get('https://jsonplaceholder.typicode.com/photos?_limit=1')
 	}
 }
